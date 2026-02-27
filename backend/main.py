6502ad94690b0bd2ae2bfe8c9d8a57885d86a5ce
Supradin4ik <supradin4ik2@gmail.com>
2026-02-27 15:02:25 +0400

    Open CORS policy and pin frontend API URL

--- backend/main.py
+++ backend/main.py
@@ -32,11 +32,11 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = ["http://localhost:3000"]
+origins = ["*"]  # Разрешаем запросы из любого источника (для теста)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
